80d0255cd241f651b2032de26129e00b7a835cb3
GwangrokBaek <zester926@gmail.com>
2022-03-03 00:08:53 +0900

    feat: command 관련 이벤트 콜백 base 구현

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "leetup" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("leetup.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello VS Code");
+  let submit = vscode.commands.registerCommand("leetup.submit", () => {
+    vscode.window.showInformationMessage("Submit!!");
   });
 
-  let getDaily = vscode.commands.registerCommand("leetup.daily", () => {
-    vscode.window.showWarningMessage("Get Daily Problem");
+  let login = vscode.commands.registerCommand("leetup.login", () => {
+    vscode.window.showInformationMessage("Login!!");
   });
 
-  context.subscriptions.push(disposable);
+  let logout = vscode.commands.registerCommand("leetup.logout", () => {
+    vscode.window.showInformationMessage("Logout!!");
+  });
+
+  context.subscriptions.push(submit);
+  context.subscriptions.push(login);
+  context.subscriptions.push(logout);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
