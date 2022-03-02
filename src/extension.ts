e1c6d3413a2904f8cf90c44d679af24e9e52bdb1
GwangrokBaek <zester926@gmail.com>
2022-03-03 00:29:40 +0900

    view에 when clause 추가

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "leetup" is now active!');
+  vscode.commands.executeCommand("setContext", "leetup.userInfo", false);
 
   let submit = vscode.commands.registerCommand("leetup.submit", () => {
     vscode.window.showInformationMessage("Submit!!");
@@ -9,10 +10,12 @@ export function activate(context: vscode.ExtensionContext) {
 
   let login = vscode.commands.registerCommand("leetup.login", () => {
     vscode.window.showInformationMessage("Login!!");
+    vscode.commands.executeCommand("setContext", "leetup.userInfo", true);
   });
 
   let logout = vscode.commands.registerCommand("leetup.logout", () => {
     vscode.window.showInformationMessage("Logout!!");
+    vscode.commands.executeCommand("setContext", "leetup.userInfo", false);
   });
 
   context.subscriptions.push(submit);
